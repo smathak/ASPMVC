f3aec434b32603555252ca7582d97b39aae6c0eb
jieun kim <poiujkl9875@hanmail.net>
2019-03-24 04:56:28 +0900

    This is my first ASP.NET MVC Web page

---
+++ Vidly/Vidly/Controllers/MovieController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models; 
+
+namespace Vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Harry Potter" }; // Vidly.Models 를 추가해줘야 함. 
+            return View(movie); // Random 이라는 View 를 만든다.   
+        }
+    }
+}
